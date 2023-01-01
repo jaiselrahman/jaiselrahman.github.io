45b097cdfedf4fcf7866f4163affe44311e6a7ef
Jaisel Rahman <jaiselrahman@gmail.com>
2023-01-01 13:28:30 +0530

    chore: update package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: { alias: {'@': '/src'} },
-  plugins: [vue()]
+  resolve: {alias: {'@': '/src'}},
+  plugins: [vue()],
 })
